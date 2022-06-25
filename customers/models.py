12704a98846924cd848c9fe604246259abacbc31
mitsekVasyl <mitsek.vasyl@gmail.com>
2022-06-25 13:22:26 +0300

    Create Customer model, apply migrations for it

--- customers/models.py
+++ customers/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Customer(models.Model):
+
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=254)
+    phone = models.CharField(max_length=15)
+    address = models.TextField(blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
